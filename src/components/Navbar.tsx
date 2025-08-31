267114fa901e7aa041111c19bbe548b0a2b4007d
Jade Menezes <jaderegis97@gmail.com>
2025-08-31 17:57:15 -0300

    Created Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { IoLanguage } from 'react-icons/io5';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-white navbar-dark bg-success">
+      {/* Justify-content-smth (start (left), center, end (right)) */}
+      <div className="container-fluid ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav gap-2">
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Início
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Programação
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Sobre
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Organização
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Inscrições
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
