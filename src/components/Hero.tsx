84cc14fe26b7d8e0bb1cfd3e0e64c9f1ef4f2895
Jade Menezes <jaderegis97@gmail.com>
2025-09-09 16:37:22 -0300

    Refactored css + added key to About page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,30 @@
 const Hero = () => {
   return (
-    <div className="hero-container d-flex justify-content-center align-items-center p-3">
+    <div className="hero-container d-flex justify-content-center align-items-center p-2 border border-primary">
       <div className="row row-cols-1 row-cols-lg-2 align-items-center">
-        <div className="">
-          <h1 className="title p-2 mb-lg-5">
+        <div className="text-container">
+          <h1 className="title p-2 mb-lg-5 border border-secondary">
             Bem vind@s ao II Colóquio Internacional e ao IV Colóquio de Pesquisa
             Social Crítico-dialética
           </h1>
 
-          <div className="d-flex justify-content-between me-md-5">
+          <div className="d-flex justify-content-between me-md-5 border border-success">
             <button className="btn btn-secondary hero-btn m-2">Sobre</button>
             <button className="btn btn-success hero-btn m-2">
               Faça sua inscrição!
             </button>
           </div>
-          <p className="inf-paragraph py-2 px-2 mt-2">
+          <p className="inf-paragraph py-2 px-2 mt-2 border border-danger">
             As inscrições dos trabalhos serão realizadas no período de
             01/08/2025 a 10/10/2025
           </p>
         </div>
         <div className="hero-img-frame">
-          <img className="img-fluid" src="images/img3.jpg" alt="" />
+          <img
+            className="img-fluid"
+            src="/cpscdiv-web/images/img3.jpg"
+            alt=""
+          />
         </div>
       </div>
     </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,9 @@ const About = () => {
     <div className="container py-5">
       <div className="row">
         <div className="col-12">
-          {aboutData.map((data) => (
+          {aboutData.map((data, index) => (
             <AboutCard
+              key={index}
               image={data.image}
               title={data.title}
               content={data.content}
